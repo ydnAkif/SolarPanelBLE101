4620d960cf13a1e07b7773f31f57c353dd6acd6f
Akif AYDIN <62480620+ydnAkif@users.noreply.github.com>
2024-05-05 17:25:44 +0300

    Updated with scrolling text animation

--- Arduino_101_LCD_Bluetooth.ino
+++ Arduino_101_LCD_Bluetooth.ino
@@ -4,15 +4,24 @@
 #include <LiquidCrystalTr_I2C.h>
 #include <CurieBLE.h>
 
+
 // Define LCD parameters
-LiquidCrystal_I2C lcd(0x3f, 16, 2);  // I2C address, rows, columns
+int lcd_Columns = 16;  // Number of columns in display
+int lcd_Rows = 2;      // Number of Rows in display
+
+LiquidCrystal_I2C lcd(0x3f, lcd_Columns, lcd_Rows);  // I2C address, rows, columns
+
+// Animation parameters
+int speed_Adjust = 300;  // Speed of moving Text
+int rest_Time = 400;     // Resting time of Text animation at the edges of display
+int text_Len;            // Variable to save text length
 
 // BLE peripheral and service
 BLEPeripheral blePeripheral;
 BLEService lcdService("19B10000-E8F2-537E-4F6C-D104768A1214");  // Custom UUID for LCD service
 
 // BLE characteristic to hold the text
-BLEStringCharacteristic textCharacteristic("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite,16);
+BLEStringCharacteristic textCharacteristic("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 255);
 
 void setup() {
   // Serial begin()
@@ -46,11 +55,36 @@ void loop() {
       // Check if new text is received
       if (textCharacteristic.written()) {
         String receivedText = textCharacteristic.value();
+        text_Len = receivedText.length();
 
-        // Clear screen and display received text (truncate if too long)
-        lcd.clear();
-        lcd.print(receivedText.substring(0, 16));
-        Serial.println(receivedText);
+        for (int j = 0; j < lcd_Rows; j++) {
+          for (int i = 0; i < (lcd_Columns - text_Len + 1); i++) {
+            lcd.clear();
+            lcd.setCursor(i, j);
+            lcd.print(receivedText);
+            delay(speed_Adjust);
+          }
+
+          delay(rest_Time);
+
+          for (int i = (lcd_Columns - text_Len); i > -1; i--) {
+            lcd.clear();
+            lcd.setCursor(i, j);
+            lcd.print(receivedText);
+            delay(speed_Adjust);
+          }
+
+          delay(rest_Time);
+        }
+
+        for (int j = lcd_Rows - 1; j >= 0; j--) {
+          lcd.clear();
+          lcd.setCursor(0, j);
+          lcd.print(receivedText);
+          delay(speed_Adjust);
+        }
+        
+        delay(rest_Time);
       }
     }
   }
